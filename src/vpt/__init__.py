44d7ad5ec3bb0ac7fc843c0f1f361b9f5e14e20b
Armijn Hemel <armijn@tjaldur.nl>
2022-03-03 13:57:56 +0100

    rename Node to Tree, as it better reflects the data structure

--- src/vpt/__init__.py
+++ src/vpt/__init__.py
@@ -25,7 +25,7 @@ import statistics
 import tlsh
 
 
-class Node:
+class Tree:
     '''Vantage point tree'''
     def __init__(self, tobj, threshold=0):
         self.left_child = None
@@ -40,8 +40,8 @@ def vpt_grow(tlsh_object_list):
     vantage_point_object = tlsh_object_list[0]
 
     if lenList == 1:
-        thisNode = Node(vantage_point_object, -1)
-        return thisNode
+        thisTree = Tree(vantage_point_object, -1)
+        return thisTree
 
     # compute the TLSH distances for each object
     distances_list = [vantage_point_object.diff(h1) for h1 in tlsh_object_list]
@@ -49,7 +49,7 @@ def vpt_grow(tlsh_object_list):
     # determine the median
     med = statistics.median_low(distances_list)
 
-    thisNode = Node(vantage_point_object, med)
+    thisTree = Tree(vantage_point_object, med)
 
     # split data into two lists: left child and right
     # child depending on the TLSH distance to vantage_point_object
@@ -65,40 +65,40 @@ def vpt_grow(tlsh_object_list):
 
     # recursively walk the data, unless there is no more data
     if tlsh_objects_left != []:
-        thisNode.left_child = vpt_grow(tlsh_objects_left)
+        thisTree.left_child = vpt_grow(tlsh_objects_left)
     else:
-        thisNode.left_child = None
+        thisTree.left_child = None
     if tlsh_objects_right != []:
-        thisNode.right_child = vpt_grow(tlsh_objects_right)
+        thisTree.right_child = vpt_grow(tlsh_objects_right)
     else:
-        thisNode.right_child = None
-    return thisNode
+        thisTree.right_child = None
+    return thisTree
 
 extra_constant = 20
 
-def vpt_search(node, search_item, best):
+def vpt_search(tree, search_item, best):
     '''Search a TLSH hash in a VPT'''
-    if node is None:
+    if tree is None:
         return
 
     # compute the TLSH distance between the item
-    # to be searched and the node of the tree
-    d = search_item.diff(node.tobj)
+    # to be searched and the tree of the tree
+    d = search_item.diff(tree.tobj)
     if d < best['dist']:
         best['dist'] = d
-        best['hash'] = node.tobj.hexdigest()
+        best['hash'] = tree.tobj.hexdigest()
 
     if d == 0:
         return best
 
-    if d <= node.threshold:
-        best_child = vpt_search(node.left_child, search_item, best)
-        if (d + best['dist'] + extra_constant) >= node.threshold:
-            best_child = vpt_search(node.right_child, search_item, best)
+    if d <= tree.threshold:
+        best_child = vpt_search(tree.left_child, search_item, best)
+        if (d + best['dist'] + extra_constant) >= tree.threshold:
+            best_child = vpt_search(tree.right_child, search_item, best)
     else:
-        best_child = vpt_search(node.right_child, search_item, best)
-        if (d - best['dist'] - extra_constant) <= node.threshold:
-            best_child = vpt_search(node.left_child, search_item, best)
+        best_child = vpt_search(tree.right_child, search_item, best)
+        if (d - best['dist'] - extra_constant) <= tree.threshold:
+            best_child = vpt_search(tree.left_child, search_item, best)
 
     if best_child is not None:
         if best_child['dist'] < best['dist']:
@@ -111,17 +111,17 @@ def pickle_tree(root, pickle_file):
     pickle_prep(root)
     pickle.dump(root, pickle_file)
 
-def pickle_prep(node):
-    if node:
-        node.tobj = node.tobj.hexdigest()
-        pickle_prep(node.left_child)
-        pickle_prep(node.right_child)
+def pickle_prep(tree):
+    if tree:
+        tree.tobj = tree.tobj.hexdigest()
+        pickle_prep(tree.left_child)
+        pickle_prep(tree.right_child)
 
-def pickle_restore(node):
-    if node:
+def pickle_restore(tree):
+    if tree:
         h = tlsh.Tlsh()
-        h.fromTlshStr(node.tobj)
-        node.tobj = h
-        pickle_restore(node.left_child)
-        pickle_restore(node.right_child)
-        return node
+        h.fromTlshStr(tree.tobj)
+        tree.tobj = h
+        pickle_restore(tree.left_child)
+        pickle_restore(tree.right_child)
+        return tree
