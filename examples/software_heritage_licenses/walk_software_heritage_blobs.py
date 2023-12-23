db8aa62dc8d54bc94b9ba7926fa015553284dfce
Armijn Hemel <armijn@tjaldur.nl>
2023-12-23 16:38:14 +0100

    walk_software_heritage_blobs.py: pylint-3 fixes, add some comments

--- examples/software_heritage_licenses/walk_software_heritage_blobs.py
+++ examples/software_heritage_licenses/walk_software_heritage_blobs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #
 # Copyright 2022 - Armijn Hemel
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import os
-import sys
 import csv
 import hashlib
 import pathlib
@@ -28,10 +26,9 @@ import click
 
 @click.command(short_help='process')
 @click.option('--output', '-o', required=True, help='path to output CSV file', type=click.File('w'))
-@click.option('--directory', '-d', required=True, help='path to samples directory', type=click.Path(exists=True))
+@click.option('--directory', '-d', required=True, help='path to samples directory',
+              type=click.Path(exists=True, path_type=pathlib.Path))
 def main(output, directory):
-    directory = pathlib.Path(directory)
-
     fieldnames = ['sha1', 'sha256', 'tlsh', 'normalized_sha256', 'normalized_tlsh']
     writer = csv.DictWriter(output, fieldnames=fieldnames)
     writer.writeheader()
@@ -44,9 +41,9 @@ def main(output, directory):
             continue
         try:
             sha1sum = sample.name
-            with sample.open(mode='r') as p:
+            with sample.open(mode='r') as license_file:
                 # read the data
-                data = p.read()
+                data = license_file.read()
 
                 # tokenize to get rid of all the whitespace
                 tokens = data.split()
@@ -57,22 +54,24 @@ def main(output, directory):
                 # convert to bytes
                 binary_license_data = license_data.encode()
 
-                # compute sha256 and tlsh hashes
+                # compute tlsh hash
                 normalized_tlsh_hash = tlsh.hash(binary_license_data)
 
-            if normalized_tlsh_hash != 'TNULL' and normalized_tlsh_hash != '':
+            # compute SHA256 but only in case if there is a valid TLSH
+            if normalized_tlsh_hash not in ['TNULL', '']:
                 sha256sum = hashlib.new('sha256')
                 sha256sum.update(binary_license_data)
                 normalized_sha256 = sha256sum.hexdigest()
 
-                with sample.open(mode='rb') as p:
+                with sample.open(mode='rb') as license_file:
                     # reread original data (as binary) and compute sha256
-                    data = p.read()
+                    data = license_file.read()
                     sha256sum = hashlib.new('sha256')
                     sha256sum.update(data)
                     tlsh_hash = tlsh.hash(data)
-                writer.writerow({'sha1': sha1sum, 'sha256': sha256sum.hexdigest(), 'tlsh': tlsh_hash,
-                                 'normalized_sha256': normalized_sha256, 'normalized_tlsh': normalized_tlsh_hash})
+                writer.writerow({'sha1': sha1sum, 'sha256': sha256sum.hexdigest(),
+                                 'tlsh': tlsh_hash, 'normalized_sha256': normalized_sha256,
+                                 'normalized_tlsh': normalized_tlsh_hash})
         except:
             pass
 
